10c231a5dd8614509c4be9c8d9ff7b939b75e92d
Yw-xwx <2330364345@qq.com>
2022-09-03 20:49:37 +0800

    how to commit in VSCode

---
+++ steps_in_VSCode.c
@@ -0,0 +1,11 @@
+/*
+    ctrl+shift+G-----into the control of source code
+    the tail of file in its line:
+        U means untracked,press "+" button means: "git add"
+        A means added,
+        next input the message of the submit in the top of this folders
+        and press "√"
+    
+    PS:
+        defaultedly, we are in the master branch
+*/
